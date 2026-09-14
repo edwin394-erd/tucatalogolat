d84c59215871f7d6e513199d6977c907ff578164
edwin394-erd <130197227+edwin394-erd@users.noreply.github.com>
2026-09-14 00:38:29 -0400

    update title (tucatalgolat.lat)

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,7 +7,7 @@
      <link rel="icon" type="image/png" href="{{ asset('imgs/icono.ico') }}" />
        <link rel="shortcut icon" href="{{ asset('imgs/icono.ico') }}" />
        <link rel="apple-touch-icon" href="{{ asset('imgs/icono.ico') }}" />
-    <title>tucatalogo.lat</title>
+    <title>tucatalogolat.lat</title>
   @vite(['resources/css/app.css', 'resources/js/app.js'])
   @livewireStyles
     <style>
