679146c889d5f53c8e3a092f412acbecb4b9557d
edwinprobando-prog <edwinprobando@gmail.com>
2026-09-17 14:28:53 -0400

    fix

--- resources/views/livewire/catalogo2.blade.php
+++ resources/views/livewire/catalogo2.blade.php
@@ -160,7 +160,7 @@
             </div>
 
             {{-- Grid --}}
-            <div class="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 gap-2 md:gap-3">
+            <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 gap-2 md:gap-3">
                 @forelse ($catalogo->products as $item)
                     <x-product-card :catalogo="$catalogo" :item="$item" :iconColor="$iconColor" />
                 @empty
